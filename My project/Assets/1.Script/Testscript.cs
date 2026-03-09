5c073161efa318416798bde38aa771fae883f258
gimyungyu9-afk <gimyungyu9@gmail.com>
2026-03-09 21:28:27 +0900

    채인지

---
+++ My project/Assets/1.Script/Testscript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testscript : MonoBehaviour
+{
+    private stirng name = "김윤규";
+    private int age = 26;
+    private string MBTI = "INFJ";
+
+    void Start()
+    {
+        SelfIntroduction();
+        Selfintroduction(name, age, MBTI);
+    }
+    void SelfIntroduction()
+    {
+        Debug.Log("안녕하세요. 저는 " + name + "입니다.");
+        Debug.Log("나이는 " + age + "살입니다.");
+        Debug.Log("MBTI는 " + MBTI + "입니다.");
+    }
+
+    void Selfintroduction(string name, int age, string MBTI)
+    {
+        Debug.Log("안녕하세요. 저는 " + name + "입니다.");
+        Debug.Log("나이는 " + age + "살입니다.");
+        Debug.Log("MBTI는 " + MBTI + "입니다.");
+    }
+}
+
